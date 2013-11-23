bed626c2be1e89eea500f67f8b71f93d3cb1badc
Benjamin Kellogg <benk691@gmail.com>
2013-11-23 03:55:27 -0800

    DB schemas for the user and room (tmp scheamas)

--- app.js
+++ app.js
@@ -30,17 +30,32 @@ client.connect(function(err) {
   });
 });
 
-/*
-user_query = client.query("SELECT * FROM User");
+// Drop tables that exist
+client.query("DROP TABLE IF EXISTS User;");
+client.query("DROP TABLE IF EXISTS Room;");
 
+// Create the tables
+user_query = client.query("CREATE TABLE User(firstname varchar(50) NOT NULL, lastname varchar(50) NOT NULL, username varchar(50) PRIMARY KEY, password varchar(50) NOT NULL, room_id bigserial FOREIGN KEY REFERENCES Room(room_id);");
+
+room_query = client.query("CREATE TABLE Room(room_id bigserial PRIMARY KEY, username varchar(50) FOREIGN KEY REFERENCES User(username) NOT NULL);");
+
+client.query("INSERT INTO User VALUES('Ben', 'Kellogg', 'bkell001', '1234', 1);");
+
+user_query = client.query("SELECT * FROM User;");
+
+util.log(user_query);
+
+/*
 user_query.on("row", function (row, result) {
     result.addRow(row);
 });
+*/
 
+/*
 user_query.on("end", function(result) {
     console.log(JSON.stringify(result.rows, null, "   "));
     client.end();
-}
+});
 */
 
 /*****************************************************/
