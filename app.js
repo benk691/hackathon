6d850c0cd7c17f97020ebc170c2d151bd69f25b2
Benjamin Kellogg <benk691@gmail.com>
2013-11-23 05:04:27 -0800

    Update to insertions of DB

--- app.js
+++ app.js
@@ -28,22 +28,58 @@ client.connect(function(err) {
     }
     console.log(result.rows[0].theTime);
   });
+
+/*
+// Drop tables that exist
+client.query("DROP TABLE IF EXISTS GSD_User CASCADE", function(err, result) {
+    if(err) {
+        return console.error('error running query', err);
+    }
 });
 
-// Drop tables that exist
-client.query("DROP TABLE IF EXISTS User;");
-client.query("DROP TABLE IF EXISTS Room;");
+client.query("DROP TABLE IF EXISTS GSD_Room CASCADE;", function(err, result) {
+    if(err) {
+        return console.error('error running query', err);
+    }
+});
+
 
 // Create the tables
-user_query = client.query("CREATE TABLE User(firstname varchar(50) NOT NULL, lastname varchar(50) NOT NULL, username varchar(50) PRIMARY KEY, password varchar(50) NOT NULL, room_id bigserial FOREIGN KEY REFERENCES Room(room_id);");
+user_query = client.query("CREATE TABLE GSD_User(firstname varchar(50) NOT NULL, lastname varchar(50) NOT NULL, username varchar(50) PRIMARY KEY, password varchar(50) NOT NULL", function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+  });
 
-room_query = client.query("CREATE TABLE Room(room_id bigserial PRIMARY KEY, username varchar(50) FOREIGN KEY REFERENCES User(username) NOT NULL);");
+room_query = client.query("CREATE TABLE GSD_Room(room_id bigserial PRIMARY KEY)", function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+  });
 
-client.query("INSERT INTO User VALUES('Ben', 'Kellogg', 'bkell001', '1234', 1);");
+group_query = client.query("CREATE TABLE GSD_Group(room_id bigserial FOREIGN KEY REFERENCES Room(room_id) NOT NULL, username varchar(50) FOREIGN KEY REFERENCES User(username) NOT NULL)", function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+  });
+*/
 
-user_query = client.query("SELECT * FROM User;");
+client.query("INSERT INTO GSD_User(firstname, lastname, username, password) VALUES($1, $2, $3, $4);", ["Ben", "K", "bkell001", "1234"], function(err, result) {
+    if(err) {
+        return console.error('error running query', err);
+    }
+});
+/*
+sel_user_query = client.query("SELECT * FROM GSD_User", function(err, result) {
+    if(err) {
+        return console.error('error running query', err);
+    }
+    
+});;
 
-util.log(user_query);
+util.log(sel_user_query);
+*/
+});
 
 /*
 user_query.on("row", function (row, result) {
