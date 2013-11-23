ec5fe81d36bad4aa53e08eca2989831bd3704b4f
Benjamin Kellogg <benk691@gmail.com>
2013-11-23 01:44:08 -0800

    Stuff

--- app.js
+++ app.js
@@ -16,20 +16,32 @@ var credentials = require('./database/credentials.js').data;
 // Client
 var client = new pg.Client(credentials);
 
-// Connect to the server
+// Connect to the server and report any error
 client.connect(function(err) {
   if(err) {
     return console.error('could not connect to postgres', err);
   }
+  // Query time of connect
   client.query('SELECT NOW() AS "theTime"', function(err, result) {
     if(err) {
       return console.error('error running query', err);
     }
     console.log(result.rows[0].theTime);
-    //output: Tue Jan 15 2013 19:12:47 GMT-600 (CST)
-    client.end();
   });
 });
+
+/*
+user_query = client.query("SELECT * FROM User");
+
+user_query.on("row", function (row, result) {
+    result.addRow(row);
+});
+
+user_query.on("end", function(result) {
+    console.log(JSON.stringify(result.rows, null, "   "));
+    client.end();
+}
+*/
 
 /*****************************************************/
 
